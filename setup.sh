a985f77ebc2fc412c553899f183d9f06251f60f4
JochiSt <jochen.steinmann@web.de>
2022-04-21 08:29:36 +0200

    added setup of PATH variable
    included both ARM and RISCV GCC

--- setup.sh
+++ setup.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+
 export AI_PROJECT_ROOT=`pwd`
+
+# Handle ARM GCC
+ARM_GCC_DIR=$AI_PROJECT_ROOT
+ARM_GCC_DIR+="/"
+ARM_GCC_DIR+="gcc-arm-none-eabi-9-2019-q4-major"
+
+echo $PATH | grep -q -s "$ARM_GCC_DIR/bin"
+if [ $? -eq 1 ] ; then
+    PATH=$PATH:$ARM_GCC_DIR/bin
+    export PATH
+    ARMGCC_DIR=$ARM_GCC_DIR
+    export ARMGCC_DIR
+fi
+
+# Handle RISCV GCC
+RISCV_GCC_DIR=$AI_PROJECT_ROOT
+RISCV_GCC_DIR+="/"
+RISCV_GCC_DIR+="xPacks/riscv-none-embed-gcc/8.3.0-1.1"
+
+echo $PATH | grep -q -s "$RISCV_GCC_DIR/bin"
+if [ $? -eq 1 ] ; then
+    PATH=$PATH:$RISCV_GCC_DIR/bin
+    export PATH
+    RISCVGCC_DIR=$RISCV_GCC_DIR
+    export RISCVGCC_DIR
+fi
