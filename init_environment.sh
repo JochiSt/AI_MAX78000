c528e9f03f2e10c0e6f1c14cd789cba9f5d91721
JochiSt <jochen.steinmann@web.de>
2022-04-21 15:58:53 +0200

    if python > 3.7 there is no need to install different packages

--- init_environment.sh
+++ init_environment.sh
@@ -59,11 +59,11 @@ pip3 install --upgrade pip
 pip3 install -U pip wheel setuptools
 pip3 install -r requirements.txt
 
-pip3 install matplotlib
+#pip3 install matplotlib
 
 # install pytorch for generating the examples
-# somehow gen-tf-demos-max78000.sh needs pyTorch
-pip3 install torch
+# somehow gen-tf-demos-max78000.sh needs pyTorch and some other packages
+#pip3 install torch colorama onnx onnxruntime pyyaml yamllint
 
 # deactivate environment
 deactivate
