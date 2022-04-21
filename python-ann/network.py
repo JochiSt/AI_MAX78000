93e29997cc7d5239576ffed704bab1739a0d3d22
JochiSt <jochen.steinmann@web.de>
2022-04-21 13:01:20 +0200

    slightly changed network structure
    each dense layer is followed by normalisation and dropout

--- python-ann/network.py
+++ python-ann/network.py
@@ -18,28 +18,55 @@ def create_model(name="network_MAX78000_v0.1"):
     inputs = keras.Input(shape=(128,), name="waveform_input")
 
     layer_cnt=0
-    x = keras.layers.Dense(8, 
+
+    ################
+    x = keras.layers.Dense(32, 
                             activation="relu",
                             kernel_regularizer=keras.regularizers.l1(0.00001),
-                            name="layer_%d"%(layer_cnt))(inputs)
+                            name="dense_%d"%(layer_cnt))(inputs)
     layer_cnt+=1
 
-    x = keras.layers.Dropout(0.005)(x)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Dense(8, 
+    x = keras.layers.Dropout(0.005,
+                            name="dropout_%d"%(layer_cnt))(x)
+    layer_cnt+=1
+
+    x = tf.keras.layers.BatchNormalization(
+                            name="norm_%d"%(layer_cnt))(x)
+    layer_cnt+=1
+
+    ################
+
+    x = keras.layers.Dense(32, 
                             activation="relu",
                             kernel_regularizer=keras.regularizers.l1(0.00001),
-                            name="layer_%d"%(layer_cnt))(x)
+                            name="dense_%d"%(layer_cnt))(x)
     layer_cnt+=1
 
-    x = keras.layers.Dropout(0.005)(x)
-    x = keras.layers.BatchNormalization()(x)
-    x = keras.layers.Dense(8, 
+    x = keras.layers.Dropout(0.005,
+                            name="dropout_%d"%(layer_cnt))(x)
+    layer_cnt+=1
+
+    x = tf.keras.layers.BatchNormalization(
+                            name="norm_%d"%(layer_cnt))(x)
+    layer_cnt+=1
+
+    ################
+
+    x = keras.layers.Dense(32, 
                             activation="relu",
                             kernel_regularizer=keras.regularizers.l1(0.00001),
-                            name="layer_%d"%(layer_cnt))(x)
+                            name="dense_%d"%(layer_cnt))(x)
     layer_cnt+=1
 
+    x = keras.layers.Dropout(0.005,
+                            name="dropout_%d"%(layer_cnt))(x)
+    layer_cnt+=1
+
+    x = tf.keras.layers.BatchNormalization(
+                            name="norm_%d"%(layer_cnt))(x)
+    layer_cnt+=1
+
+    ##################
     # final layer for classification
     outputs = keras.layers.Dense(3, name="classification")(x)
 
